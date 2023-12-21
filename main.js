d60f88df25f002ddd1c0fa2d8b0721a034894985
Timur-Git <uchebapraktikum-t1@yandex.ru>
2023-12-21 15:41:22 +0300

    Fix mistakes

--- main.js
+++ main.js
@@ -8,9 +8,7 @@ function toggleMenuVisibility() {
 menuBtn.addEventListener("click", toggleMenuVisibility);
 
 //Скрипт для раскрытия блока portfolio
-//const portfolioBtn = document.querySelector(".portfolio__btn");
 const portfolioBtn = document.getElementById("portfolio__btn__id")
-//const portfolioList = document.querySelector(".portfolio__list");
 const portfolioList = document.getElementById("portfolio__list__id");
 
 function togglePortfolioVisibility() {
